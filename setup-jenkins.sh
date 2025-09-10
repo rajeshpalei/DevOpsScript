9e6820a48ce3a89d3e86663a637759e1ea24a5da
kumar <r.palei33@gmail.com>
2025-09-10 18:18:27 +0530

    adding the sudo command on every command

--- setup-jenkins.sh
+++ setup-jenkins.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-sudo -i
 #Installing the git & maven &wget
-yum install -y maven git wget
+sudo yum install -y maven git wget
 
 # Installing the java
-yum install java-17-amazon-corretto -y
+sudo yum install java-17-amazon-corretto -y
 
 #Let Download the jenkins Repo
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 #Install the jenkins
-yum install jenkins -y
+sudo yum install jenkins -y
 
 #Start the jenkins service
-systemctl start jenkins.service
-systemctl enable jenkins.service
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
 
 
